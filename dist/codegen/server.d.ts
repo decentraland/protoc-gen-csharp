5c816ae1afdf95d3409d1f08c7b1f1327d0e56ea
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-07-13 09:58:44 -0300

    fix: remove dist folder and ignore it

--- dist/codegen/server.d.ts
+++
@@ -1,5 +0,0 @@
-import { ExportMap } from "ts-protoc-gen/lib/ExportMap";
-import { FileDescriptorProto } from "google-protobuf/google/protobuf/descriptor_pb";
-import { CodeGeneratorResponse } from "google-protobuf/google/protobuf/compiler/plugin_pb";
-export declare function generateServerRpcService(filename: string, descriptor: FileDescriptorProto, exportMap: ExportMap): CodeGeneratorResponse.File[];
-//# sourceMappingURL=server.d.ts.map
