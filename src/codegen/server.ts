f2fe7dc6fd13720a2cdaddacd955a3a9e8d536d0
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-10-27 10:41:30 -0300

    fix: separate abstract class to an interface and static class

--- src/codegen/server.ts
+++ src/codegen/server.ts
@@ -76,7 +76,7 @@ using rpc_csharp;`)
 
   // Services.
   serviceDescriptor.services.forEach((service) => {
-    const serviceClassName = `${service.name}Base`
+    const serviceNameInterface = `I${service.name}`
 
     const serviceHeaderPrinter = new Printer(0)
     const methodsPrinter = new Printer(0)
@@ -91,7 +91,7 @@ using rpc_csharp;`)
       serviceHeaderPrinter.print(`, ${method.nameAsPascalCase} ${method.nameAsCamelCase}`)
 
       methodsPrinter.printEmptyLn()
-      methodsPrinter.printIndentedLn(`protected abstract ${responseTypeEx} ${method.nameAsPascalCase}(${requestTypeEx} ${requestVarName}, Context context${!method.responseStream? ", CancellationToken ct":""});`)
+      methodsPrinter.printIndentedLn(`protected ${responseTypeEx} ${method.nameAsPascalCase}(${requestTypeEx} ${requestVarName}, Context context${!method.responseStream? ", CancellationToken ct":""});`)
 
       if (method.responseStream && method.requestStream) {
         registerMethodPrinter.printLn(`    result.bidirectionalStreamDefinition.Add("${method.nameAsPascalCase}", (IUniTaskAsyncEnumerable<ByteString> payload, Context context) => {`)
@@ -112,11 +112,16 @@ using rpc_csharp;`)
     })
 
     printer.print(`
-public abstract class ${serviceClassName}<Context>
+public interface class ${serviceNameInterface}<Context>
+{
+${methodsPrinter.output}
+}
+
+public static class ${service.name}CodeGen
 {
   public const string ServiceName = "${service.name}";
-${methodsPrinter.output}
-  public static void RegisterService(RpcServerPort<Context> port, ${serviceClassName}<Context> service)
+
+  public static void RegisterService(RpcServerPort<Context> port, ${serviceNameInterface}<Context> service)
   {
     var result = new ServerModuleDefinition<Context>();
       
